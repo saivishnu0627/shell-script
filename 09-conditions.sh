6c4d6ebe42ce809b386a9443fb838230aec5c4a0
saivishnu0627 <saivishnu0627@gmail.com>
2026-01-25 00:29:02 -0600

    hello script

---
+++ 09-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ NUMBER -gt 10]
+then
+    echo "Given number $NUMBER is greater then 10"
+else
+    echo "Given number $Number is less than 10"
+fi
+
+#-gt -- greater command, -lt  -- less then command, -eq -- eaual command
+#-ge -- greater then equal to command, -le -- less than equal to command
