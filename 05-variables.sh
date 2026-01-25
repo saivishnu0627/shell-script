ff38d1e21f0efb566a87cf75f0668e340210bb59
saivishnu0627 <saivishnu0627@gmail.com>
2026-01-24 23:09:23 -0600

    hello script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter username::"
+
+read -s USERNAME #here username is variable
+
+echo "please enter password::"
+
+read -s PASSWORD #here -s is hide the what you entered beacuse it's confidential
+
+echo "username is:$USERNAME, Password is:$PASSWORD"
